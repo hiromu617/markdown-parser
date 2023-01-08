e3bcae81b8b50713c068dfb685ed38cf3deee954
hiromu617 <hiromhiro617@gmail.com>
2023-01-08 15:11:14 +0900

    add test

--- src/tests/integration/convertHTMLString.test.ts
+++ src/tests/integration/convertHTMLString.test.ts
@@ -1,6 +1,50 @@
 import { convertToHTMLString } from "../..";
-import {describe, expect, test} from '@jest/globals';
+import { describe, expect, test } from "@jest/globals";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+describe("convertToHTMLString", () => {
+  describe("strong", () => {
+    // strong
+    test("**text**がstrong要素として出力される", () => {
+      const string = "**text**";
+      const expected = "<strong>text</strong>";
+      expect(convertToHTMLString(string)).toBe(expected);
+    });
+
+    test("text1**text2**が正しく変換される", () => {
+      const string = "text1**text2**";
+      const expected = "text1<strong>text2</strong>";
+      expect(convertToHTMLString(string)).toBe(expected);
+    });
+  });
+
+  describe("list", () => {
+    // list
+    test(" - list1\n - list2\n - list3がlist要素として出力される", () => {
+      const string = `
+- list1
+- list2
+- list3
+`
+      const expected = "<ul><li>list1</li><li>list2</li><li>list3</li></ul>";
+      expect(convertToHTMLString(string)).toBe(expected);
+    });
+  });
+
+  test("複雑なマークダウンの時、正しくHTMLが生成される", () => {
+    const string = `
+normal text
+
+- list1
+- list2
+- list3
+
+normal
+
+- **aaa**
+- bbb
+`;
+    const expected =
+      "normal text<ul><li>list1</li><li>list2</li><li>list3</li></ul>normal<ul><li><strong>aaa</strong></li><li>bbb</li></ul>";
+    expect(convertToHTMLString(string)).toBe(expected);
+  });
 });
